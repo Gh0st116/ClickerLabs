96093e5e8d625fc44852b4ce4645932743c34664
Gh0st116 <66926881+Gh0st116@users.noreply.github.com>
2022-06-07 19:46:37 -0300

    Remoção de indentação em excesso

--- Integrador/src/persistance/DBConfigs.java
+++ Integrador/src/persistance/DBConfigs.java
@@ -1,19 +1,9 @@
 package persistance;
 
-public class DBConfigs {
-	
-
-	
-	public static final String IP = "localhost";
-	
-	public static final String PORT = "3306";
-	
+public class DBConfigs {	
+	public static final String IP = "localhost";	
+	public static final String PORT = "3306";	
 	public static final String LOGIN = "root";
-
-	public static final String PASSWORD = "107709475163";
-	
-	public static final String NAME_DB = "clickers_lab";
-
-	
-	
+	public static final String PASSWORD = "senha";	
+	public static final String NAME_DB = "clickers_lab";	
 }
