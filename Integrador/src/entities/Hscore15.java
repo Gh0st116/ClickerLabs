03984ea0b95309dacd10ca533ffa1b7c6cd766e6
Gh0st116 <66926881+Gh0st116@users.noreply.github.com>
2022-06-07 20:06:16 -0300

    Remoção de indentação em excesso

--- Integrador/src/entities/Hscore15.java
+++ Integrador/src/entities/Hscore15.java
@@ -9,44 +9,30 @@ public class Hscore15 extends Hscore {
 	
 	// relacionamento 1:N com User
 	private List<User> user;
-	
-	
+		
 	// construtor vazio
 	public Hscore15() {
-		super();
-		
-		this.id15 = null;
-		
-		this.user = new ArrayList<User>();
-		
+		super();		
+		this.id15 = null;		
+		this.user = new ArrayList<User>();		
 	}
-
 	
 	// construtor com parametros
 	public Hscore15(String player_name, Long player_score, double time, double averageCps, Long id15, List<User> user) {
-		this.playerUsername = player_name;
-		
-		this.playerScore = player_score;
-		
-		this.time = time;
-		
-		this.averageCps = averageCps;
-		
-		this.id15 = id15;
-		
-		this.user = user;
-		
+		this.playerUsername = player_name;		
+		this.playerScore = player_score;		
+		this.time = time;		
+		this.averageCps = averageCps;		
+		this.id15 = id15;		
+		this.user = user;		
 	}
-
 	
 	// metodo toString
 	@Override
 	public String toString() {
 		return "Hscore15 [playerUsername = " + playerUsername + ", playerScore = " + playerScore + ", time = " + time + ", averageCps = "
 				+ averageCps +", id15 = " + id15 + ", user = " + user + "]";
-	}
-	
-	
+	}	
 
 	// getters e setters
 	public Long getId15() {
@@ -64,5 +50,4 @@ public class Hscore15 extends Hscore {
 	public void setUser(List<User> user) {
 		this.user = user;
 	}
-	
 }
