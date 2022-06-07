45602861ee2ff6c85230d0e471e374a51c1c5ca2
Gh0st116 <66926881+Gh0st116@users.noreply.github.com>
2022-06-07 19:45:39 -0300

    Remoção de indentação em excesso

--- Integrador/src/persistance/ConnectionMySQL.java
+++ Integrador/src/persistance/ConnectionMySQL.java
@@ -6,83 +6,49 @@ import java.sql.SQLException;
 
 public class ConnectionMySQL {
 	
-	private String ip;
-	
-	private String port;
-	
-	private String login;
-	
-	private String password;
-	
-	private String nameDB;
-	
+	private String ip;	
+	private String port;	
+	private String login;	
+	private String password;	
+	private String nameDB;	
 
-	private Connection connection;
-	
+	private Connection connection;	
 	
 	//construtor
-	public ConnectionMySQL(String ip, String port, String login, String password, String nameDB) {
-		
+	public ConnectionMySQL(String ip, String port, String login, String password, String nameDB) {		
 		this.ip = ip;
 		this.port = port;
 		this.login = login;
 		this.password = password;
-		this.nameDB = nameDB;
-		
+		this.nameDB = nameDB;		
 	}
-
 	
 	public Connection getConnection() {
 		return connection;
-	}
-	
+	}	
 
 	//abrir conexao com o banco de dados
-	public void openConnection() {
-		
-		try {
-			
-			
+	public void openConnection() {		
+		try {			
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			String address = "jdbc:mysql://" + this.ip + ":" + this.port + "/" + this.nameDB + "?serverTimezone=UTC";
 			
-			this.connection = DriverManager.getConnection(address, this.login, this.password);
+			this.connection = DriverManager.getConnection(address, this.login, this.password);			
 			
-			
-		} catch (ClassNotFoundException | SQLException e) {
-			
+		} catch (ClassNotFoundException | SQLException e) {			
 			e.printStackTrace();
-		}
-		
-	}
-	
+		}		
+	}	
 	
 	//fechar conexao com o BD
-	public void closeConnection() {
-		
-		try {
-			
-			if (this.connection != null) {
-				
+	public void closeConnection() {		
+		try {			
+			if (this.connection != null) {				
 				this.connection.close();
-			}
-			
+			}			
 		} catch (SQLException e) {
-
 			e.printStackTrace();
-		}
-		
-	}
-	
-	
+		}		
+	}	
 }
-
-
-
-
-
-
-
-
-
