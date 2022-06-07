d4ac5daf60814ee867b69df0cd0a4731644edf1e
Gh0st116 <66926881+Gh0st116@users.noreply.github.com>
2022-06-07 19:50:10 -0300

    Remoção de indentação em excesso

--- Integrador/src/persistance/FriendDAO.java
+++ Integrador/src/persistance/FriendDAO.java
@@ -10,20 +10,13 @@ import java.util.List;
 import entities.Friend;
 
 public class FriendDAO {
-
-
+	
 	private ConnectionMySQL connection;
 	
-	
-	
 	//construtor da classe
-	public FriendDAO() {
-		
-		this.connection = new ConnectionMySQL(DBConfigs.IP, DBConfigs.PORT, DBConfigs.LOGIN, DBConfigs.PASSWORD, DBConfigs.NAME_DB);
-		
+	public FriendDAO() {		
+		this.connection = new ConnectionMySQL(DBConfigs.IP, DBConfigs.PORT, DBConfigs.LOGIN, DBConfigs.PASSWORD, DBConfigs.NAME_DB);		
 	}
-	
-	
 	
 	//metodo adicionar
 	public Friend add(Friend friend) {
@@ -36,59 +29,35 @@ public class FriendDAO {
 		
 		Friend f = null;
 		
-		try {
-			
+		try {			
 			//preparedStatement prepara o comando do sql antes de executar
 			PreparedStatement st = this.connection.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			
 			//substituindo interrogacoes
-			st.setString(1, friend.getName());
-			
-			st.setLong(2, friend.getFpoints());
-			
-			st.setString(3, friend.getPicture());
-			
-//			st.setLong(4, friend.getScore15());
-//
-//			st.setLong(5, friend.getScore30());
-//
-//			st.setLong(6, friend.getScore60());
-//
-//			st.setLong(7, friend.getScore15());
-//
-//			st.setLong(8, friend.getScore30());
-//
-//			st.setLong(9, friend.getScore60());
-
+			st.setString(1, friend.getName());			
+			st.setLong(2, friend.getFpoints());			
+			st.setString(3, friend.getPicture());			
 			
 			ResultSet rs = st.getGeneratedKeys();
 			
-			if (rs.next()) {
-				
-				f = this.searchById(rs.getLong(1));
-				
+			if (rs.next()) {				
+				f = this.searchById(rs.getLong(1));				
 				System.out.println(f.toString());
-			}
-			
+			}			
 			
 			//executando a sql
 			st.executeUpdate();
 			
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-		
+		} catch (SQLException e) {			
+			e.printStackTrace();		
 		} finally {
 		
 			//fechando conexao
 			this.connection.closeConnection();
 		}
 		
-		return f;
-		
-	}
-	
-	
+		return f;		
+	}	
 	
 	//metodo editar
 	public void edit(Friend friend) {
@@ -99,47 +68,26 @@ public class FriendDAO {
 		//editar no bd
 		String sql = "UPDATE friend SET name = ?, fpoints = ?, picture = ? WHERE id_friend = ?;"; 
 		
-		try {
-			
+		try {			
 			PreparedStatement st = connection.getConnection().prepareStatement(sql);
 			
 			//substituindo interrogacoes
-			st.setString(1, friend.getName());
-			
-			st.setLong(2, friend.getFpoints());
-			
+			st.setString(1, friend.getName());			
+			st.setLong(2, friend.getFpoints());			
 			st.setString(3, friend.getPicture());
-			
-//			st.setLong(4, friend.getScore15());
-//			
-//			st.setLong(5, friend.getScore30());
-//
-//			st.setLong(6, friend.getScore60());
-//
-//			st.setLong(7, friend.getPos15());
-//
-//			st.setLong(8, friend.getPos30());
-//
-//			st.setLong(9, friend.getPos60());
-
 			st.setLong(10, friend.getIdFriend());
 
 			//executando sql
 			st.executeUpdate();
 			
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-			
+		} catch (SQLException e) {			
+			e.printStackTrace();			
 		} finally {
 			
 			//fechando conexao
-			this.connection.closeConnection();
-			
-		}
-			
-	}
-	
+			this.connection.closeConnection();			
+		}			
+	}	
 	
 	//metodo excluir
 	public void delete(Friend friend) {
@@ -150,8 +98,7 @@ public class FriendDAO {
 		//deletando do bd
 		String sql = "DELETE FROM friend WHERE id_friend = ?";
 		
-		try {
-			
+		try {			
 			PreparedStatement st = connection.getConnection().prepareStatement(sql);
 			
 			//substituindo interrogacoes
@@ -161,19 +108,13 @@ public class FriendDAO {
 			st.executeUpdate();
 			
 		} catch (SQLException e) {
-
-			e.printStackTrace();
-			
+			e.printStackTrace();			
 		} finally {
 			
 			//fechando conexao
 			this.connection.closeConnection();
-			
-		}
-		
-	}
-	
-	
+		}		
+	}	
 	
 	//metodo buscar por ID
 	public Friend searchById(Long idFriend) {
@@ -187,8 +128,7 @@ public class FriendDAO {
 		//buscando no bd
 		String sql = "SELECT * FROM friend WHERE id_friend = ?;";
 		
-		try {
-			
+		try {			
 			PreparedStatement st = connection.getConnection().prepareStatement(sql);
 			
 			//substituindo interrogacoes
@@ -203,33 +143,14 @@ public class FriendDAO {
 				friend = new Friend();
 				
 				//atribuindo colunas do rs a atributos do user
-				friend.setIdFriend(rs.getLong("id_friend"));
-				
-				friend.setName(rs.getString("name"));
-				
-				friend.setFpoints(rs.getLong("fpoints"));
-				
-				friend.setPicture(rs.getString("picture"));
-				
-//				friend.setScore15(rs.getLong("score15"));
-//
-//				friend.setScore30(rs.getLong("score30"));
-//
-//				friend.setScore60(rs.getLong("score60"));
-//
-//				friend.setPos15(rs.getLong("pos15"));
-//
-//				friend.setPos30(rs.getLong("pos30"));
-//
-//				friend.setPos60(rs.getLong("pos60"));
-
-			
+				friend.setIdFriend(rs.getLong("id_friend"));				
+				friend.setName(rs.getString("name"));				
+				friend.setFpoints(rs.getLong("fpoints"));				
+				friend.setPicture(rs.getString("picture"));			
 			}
 						
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-			
+		} catch (SQLException e) {			
+			e.printStackTrace();			
 		} finally {
 			
 			//fechar conexao
@@ -237,8 +158,7 @@ public class FriendDAO {
 		}
 		
 		return friend;
-	}
-	
+	}	
 	
 	//metodo buscar todos
 	public List<Friend> searchAll() {
@@ -252,10 +172,8 @@ public class FriendDAO {
 		//buscando no bd
 		String sql = "SELECT * FROM friend;";
 		
-		try {
-			
-			PreparedStatement st = connection.getConnection().prepareStatement(sql);
-			
+		try {			
+			PreparedStatement st = connection.getConnection().prepareStatement(sql);			
 			ResultSet rs = st.executeQuery();
 			
 			//converter o resultSet em um objeto Friend
@@ -265,44 +183,22 @@ public class FriendDAO {
 				Friend friend = new Friend();
 				
 				//atribuindo colunas do rs a atributos do friend
-				friend.setIdFriend(rs.getLong("id_friend"));
-				
-				friend.setName(rs.getString("name"));
-				
-				friend.setFpoints(rs.getLong("fpoints"));
-				
+				friend.setIdFriend(rs.getLong("id_friend"));				
+				friend.setName(rs.getString("name"));			
+				friend.setFpoints(rs.getLong("fpoints"));				
 				friend.setPicture(rs.getString("picture"));
-				
-//				friend.setScore15(rs.getLong("score15"));
-//
-//				friend.setScore30(rs.getLong("score30"));
-//
-//				friend.setScore60(rs.getLong("score60"));
-//
-//				friend.setPos15(rs.getLong("pos15"));
-//
-//				friend.setPos30(rs.getLong("pos30"));
-//
-//				friend.setPos60(rs.getLong("pos60"));
-								
+												
 				//adicionando friend na lista
-				listFriend.add(friend);
-				
-			}
-			
+				listFriend.add(friend);				
+			}			
 		} catch (SQLException e) {
-
-			e.printStackTrace();
-			
+			e.printStackTrace();			
 		} finally {
 			
 			//fechar conexao
-			this.connection.closeConnection();
-			
+			this.connection.closeConnection();			
 		}
 		
 		return listFriend;
-	}
-	
-	
+	}	
 }
